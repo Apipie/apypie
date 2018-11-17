021a620aa92d632f0f5dff4e6cb05f5e96387958
Evgeni Golov <evgeni@golov.de>
2018-11-17 19:08:13 +0100

    more fixtures

--- tests/test_apypie.py
+++ tests/test_apypie.py
@@ -5,6 +5,14 @@ import apypie
 def api(fixture_dir):
     return apypie.Api((fixture_dir / 'dummy.json').strpath)
 
+@pytest.fixture
+def resource(api):
+    return api.resource('users')
+
+@pytest.fixture
+def action(resource):
+    return resource.action('show')
+
 def test_init(api):
     assert api
     assert api.apidoc
@@ -13,51 +21,39 @@ def test_resources(api):
     expected = sorted(['posts', 'users', 'comments'])
     assert expected == api.resources
 
-def test_resource_existing(api):
-    resource = api.resource('users')
+def test_resource_existing(resource):
     assert 'users' == resource.name
 
 def test_resource_missing(api):
     with pytest.raises(IOError):
         api.resource('missing')
 
-def test_resource_actions(api):
-    resource = api.resource('users')
+def test_resource_actions(resource):
     expected = sorted(['index', 'show', 'create', 'update', 'destroy', 'create_unnested'])
     assert expected == resource.actions
 
-def test_resource_action(api):
-    resource = api.resource('users')
-    action = resource.action('show')
+def test_resource_action(action):
     assert 'show' == action.name
 
-def test_resource_action_missing(api):
+def test_resource_action_missing(resource):
     with pytest.raises(IOError):
-        api.resource('users').action('missing')
+        resource.action('missing')
 
-def test_action_apidoc(api):
-    resource = api.resource('users')
-    action = resource.action('show')
+def test_action_apidoc(action):
     assert action.apidoc
 
-def test_action_routes(api):
-    resource = api.resource('users')
-    action = resource.action('show')
+def test_action_routes(action):
     assert action.routes
 
-def test_action_route(api):
-    resource = api.resource('users')
-    action = resource.action('show')
+def test_action_route(action):
     route = action.routes[0]
     assert '/users/:id' == route.path
     assert 'get' == route.method
 
-def test_action_params(api):
-    resource = api.resource('users')
+def test_action_params(resource):
     action = resource.action('create')
     assert action.params
 
-def test_action_examples(api):
-    resource = api.resource('users')
+def test_action_examples(resource):
     action = resource.action('index')
     assert action.examples
