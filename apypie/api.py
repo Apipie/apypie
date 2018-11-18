f127e34f3d0518ab563bd59c33d22b00de6bb919
Evgeni Golov <evgeni@golov.de>
2018-11-18 21:10:50 +0100

    stuff

--- apypie/api.py
+++ apypie/api.py
@@ -12,6 +12,7 @@ class Api:
     def __init__(self, apifile):
         with open(apifile, 'r') as f:
             self.apidoc = json.load(f)
+            self.options = {'skip_validation': True}
 
     @property
     def resources(self):
@@ -26,15 +27,16 @@ class Api:
     def call(self, resource_name, action_name, params={}, headers={}, options={}):
         resource = Resource(self, resource_name)
         action = resource.action(action_name)
-        #action.validate!(params) unless options[:skip_validation]
-        #options[:fake_response] = find_match(fake_responses, resource_name, action_name, params) || action.examples.first if dry_run?
+        if not self.options['skip_validation']:
+            action.validate(params)
 
         self.call_action(action, params, headers, options)
 
     def call_action(self, action, params={}, headers={}, options={}):
         route = action.find_route(params)
+        get_params = dict((key, value) for key, value in params.items() if key not in route.params_in_path)
         return (
             route.method,
             route.path_with_params(params),
-            #params.reject { |par, _| route.params_in_path.include? par.to_s },
+            get_params,
             headers, options)
