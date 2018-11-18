3d08ba7e5a24e23692abad56de94d7023d315403
Evgeni Golov <evgeni@golov.de>
2018-11-18 12:02:30 +0100

    implement params in routes

--- apypie/route.py
+++ apypie/route.py
@@ -9,3 +9,19 @@ class Route:
         self.path = path
         self.method = method.lower()
         self.description = description
+
+    @property
+    def params_in_path(self):
+        return [part[1:] for part in self.path.split('/') if part.startswith(':')]
+
+    def path_with_params(self, params=None):
+        if params is None:
+            return self.path
+        else:
+            result = self.path
+            for param in self.params_in_path:
+                if param in params:
+                    result = result.replace(':{}'.format(param), str(params[param]))
+                else:
+                    raise KeyError
+            return result
