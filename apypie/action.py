8242b2fc939912cee49aa315e7bb2d38c449b3c0
Evgeni Golov <evgeni@golov.de>
2019-06-21 11:54:46 -0400

    add docstring

--- apypie/action.py
+++ apypie/action.py
@@ -96,6 +96,21 @@ class Action:
             return {}
 
     def prepare_params(self, input_dict):
+        """
+        Transform a dict with data into one that can be accepted as params for calling the action.
+
+        This will remove any keys that are not accepted as params when calling the action.
+        It also allows generating nested params without forcing the user to care about them.
+
+        :param input_dict: a dict with data that should be used to fill in the params
+        :return: :class:`dict` object
+        :rtype: dict
+
+        Usage::
+
+            >>> action.prepare_params({'id': 1})
+            {'user': {'id': 1}}
+        """
         return self._prepare_params(self.params, input_dict)
 
     def _prepare_params(self, action_params, input_dict):
