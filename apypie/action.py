0b362b9b49fae476689b771b926d9a9caa656261
Evgeni Golov <evgeni@golov.de>
2019-08-13 10:31:53 +0200

    also allow passing data, not only files (#48)
    sometimes we don't want to upload a file, but just a chunk of binary
    data

--- apypie/action.py
+++ apypie/action.py
@@ -38,8 +38,8 @@ class Action:
     def examples(self):
         return [Example.parse(example) for example in self.apidoc['examples']]
 
-    def call(self, params={}, headers={}, options={}, files=None):
-        return self.api.call(self.resource, self.name, params, headers, options, files)
+    def call(self, params={}, headers={}, options={}, data=None, files=None):
+        return self.api.call(self.resource, self.name, params, headers, options, data, files)
 
     def find_route(self, params=None):
         param_keys = set(self.filter_empty_params(params).keys())
