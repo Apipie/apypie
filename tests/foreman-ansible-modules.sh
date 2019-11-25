9a0070a64529ba4e12fa00ea5a1118caba41505e
Evgeni Golov <evgeni@golov.de>
2019-11-25 12:19:59 +0100

    only run CRUD FAM tests
    check mode should not be influenced by apypie, so let's skip that

--- tests/foreman-ansible-modules.sh
+++ tests/foreman-ansible-modules.sh
@@ -3,4 +3,4 @@
 FAM_DIR=$(mktemp -d)
 git clone --depth=1 https://github.com/theforeman/foreman-ansible-modules.git ${FAM_DIR}
 pip install ansible
-make -C ${FAM_DIR} test-setup test
+make -C ${FAM_DIR} test-setup test-crud
