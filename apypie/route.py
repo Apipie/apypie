52839bfd042a968728ff255c90fd264e2b102fbc
Evgeni Golov <evgeni@golov.de>
2020-10-07 10:20:45 +0200

    route: pylint

--- apypie/route.py
+++ apypie/route.py
@@ -1,12 +1,16 @@
+"""
+Apypie Route module
+"""
+
 from __future__ import print_function, absolute_import
 
 try:
-    from typing import List, Optional
+    from typing import List, Optional  # pylint: disable=unused-import
 except ImportError:
     pass
 
 
-class Route:
+class Route(object):
     """
     Apipie Route
     """
@@ -20,17 +24,25 @@ class Route:
     @property
     def params_in_path(self):
         # type: () -> List
+        """
+        Params that can be passed in the path (URL) of the route.
+
+        :returns: The params.
+        """
         return [part[1:] for part in self.path.split('/') if part.startswith(':')]
 
     def path_with_params(self, params=None):
         # type: (Optional[dict]) -> str
-        if params is None:
-            return self.path
-        else:
-            result = self.path
+        """
+        Fill in the params into the path.
+
+        :returns: The path with params.
+        """
+        result = self.path
+        if params is not None:
             for param in self.params_in_path:
                 if param in params:
                     result = result.replace(':{}'.format(param), str(params[param]))
                 else:
                     raise KeyError("missing param '{}' in parameters".format(param))
-            return result
+        return result
