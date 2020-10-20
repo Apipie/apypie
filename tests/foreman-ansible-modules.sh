cee45bfc61488c763e16be20559893cadceb02d4
Evgeni Golov <evgeni@golov.de>
2020-10-20 09:56:20 +0200

    update the vendored apypie in FAM during tests

--- tests/foreman-ansible-modules.sh
+++ tests/foreman-ansible-modules.sh
@@ -3,4 +3,5 @@
 FAM_DIR=$(mktemp -d)
 git clone --depth=1 https://github.com/theforeman/foreman-ansible-modules.git ${FAM_DIR}
 pip install ansible
+python ${FAM_DIR}/vendor.py ./apypie/*.py > ${FAM_DIR}/plugins/module_utils/_apypie.py
 make -C ${FAM_DIR} test-setup test-crud
