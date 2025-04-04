0c25f2108a771ba1822a175021d3029167aa4aa1
Evgeni Golov <evgeni@golov.de>
2025-04-04 11:27:13 +0200

    Release 0.7.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(path.abspath(path.dirname(__file__)), 'README.md'), encoding
 
 setup(
     name='apypie',
-    version='0.7.0',
+    version='0.7.1',
     description='Apipie bindings for Python',
     license='MIT',
     long_description=LONG_DESCRIPTION,
